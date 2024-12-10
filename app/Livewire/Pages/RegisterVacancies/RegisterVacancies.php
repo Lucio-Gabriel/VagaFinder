c5b87e71c0747c9b3a63be6af4ebaf8642afe017
Gabriel <suppershock777@gmail.com>
2024-12-10 17:00:07 -0300

    UI improvements

--- app/Livewire/Pages/RegisterVacancies/RegisterVacancies.php
+++ app/Livewire/Pages/RegisterVacancies/RegisterVacancies.php
@@ -2,11 +2,35 @@
 
 namespace App\Livewire\Pages\RegisterVacancies;
 
+
+use App\Models\Vacancy;
+use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class RegisterVacancies extends Component
 {
-    public function render()
+    #[Validate('required')]
+    public $title = '';
+    #[Validate('required')]
+    public $stacks = '';
+    #[Validate('required')]
+    public $description = '';
+
+    public function save()
+    {
+        $this->validate();
+
+        Vacancy::create(
+            $this->only(['title', 'stacks', 'description'])
+        );
+
+        session()->flash('status', 'Vaga cadastrada com sucesso');
+
+        return $this->redirect('dashboard');
+    }
+
+    public function render():View
     {
         return view('livewire.pages.register-vacancies.register-vacancies')
             ->layout('layouts.app');
